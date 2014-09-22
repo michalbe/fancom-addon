27e2f192f92e8dc5d3b6e240a5823afc6c94734f
Michał Budzyński <michal@virtualdesign.pl>
2014-09-22 01:01:00 +0100

    [FANCOM-ADDON] - :honeybee: remove emoji from PR title

--- data/script.js
+++ data/script.js
@@ -1,1 +1,2 @@
-alert('elo!');
+var PR_TITLE = document.getElementById('pull_request_title').value;
+document.getElementById('pull_request_title').value = PR_TITLE.replace(/\:([a-zA-z0-9_]+)\: /, '');
